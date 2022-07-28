293bfd4b7f6c06efabd2fc620e7431eda53c1fb3
sk8rboil <sk8rboil008@windowslive.com>
2022-07-28 13:54:45 +0700

    update profile_screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_brace_in_string_interps
+// ignore_for_file: prefer_const_constructors, unnecessary_brace_in_string_interps, unnecessary_string_interpolations
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_presence_app/controllers/profile_controller.dart';
+import 'package:my_presence_app/homepage.dart';
 
 class MyProfileScreen extends StatelessWidget {
   const MyProfileScreen({Key? key}) : super(key: key);
@@ -30,14 +31,56 @@ class MyProfileScreen extends StatelessWidget {
               return ListView(
                 // ignore: prefer_const_literals_to_create_immutables
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      radius: 50,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipOval(
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                              'https://ui-avatars.com/api/?name={$user["username"]}',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Center(child: Text("${user['username']}")),
-                  Center(child: Text("${user['email']}")),
+                  Center(
+                      child: Text(
+                    "${user['username'].toString().toUpperCase()}",
+                    style: TextStyle(fontSize: 18),
+                  )),
+                  Center(
+                      child: Text(
+                    "${user['email'].toString().toUpperCase()}",
+                    style: TextStyle(fontSize: 16),
+                  )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(Icons.person),
+                    title: Text('Update Profile'),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Get.toNamed('/resetpass_screen');
+                    },
+                    leading: Icon(Icons.vpn_key),
+                    title: Text('Update Password'),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      signOut();
+                    },
+                    leading: Icon(Icons.logout_outlined),
+                    title: Text('LOGOUT'),
+                  ),
                 ],
               );
             } else {
