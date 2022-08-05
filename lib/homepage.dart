c97d70cda6b72cbecd8c1fa62e4e719b2c52a43d
sk8rboil <sk8rboil008@windowslive.com>
2022-08-05 14:18:05 +0700

    add intl package

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_void_to_null, avoid_print, unused_local_variable
+// ignore_for_file: prefer_const_constructors, prefer_void_to_null, avoid_print, unused_local_variable, avoid_unnecessary_containers, non_constant_identifier_names
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -27,96 +27,58 @@ class MyHomePage extends GetView<HomeController> {
           ],
         ),
         body: ListView(
+          padding: EdgeInsets.all(8.0),
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  ClipOval(
-                    child: Container(
-                      height: 75,
-                      width: 75,
+            user_detail(),
+            card1(),
+            card2(),
+            Divider(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Text(
+                  'LAST 5 DAYS',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('see more'),
+                ),
+              ],
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
                       color: Colors.grey.shade300,
-                      child: Center(child: Text('data')),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      Text('WELCOME'),
-                      Text('subtitle'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey.shade300,
-                ),
-                height: 150,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: <Widget>[
-                    Text('data',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    Divider(
-                      height: 10,
-                    ),
-                    Text('123456789456123',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    Text('data'),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey.shade300,
-                ),
-                height: 75,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: <Widget>[
-                    Column(
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        Text('data'),
-                        Text('-'),
+                    height: 75,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: <Widget>[
+                            Text('data'),
+                            Text('data'),
+                          ],
+                        ),
+                        Text('${DateTime.now()}')
                       ],
                     ),
-                    VerticalDivider(
-                      width: 10,
-                    ),
-                    Column(
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        Text('data'),
-                        Text('-'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             ),
           ],
         ),
@@ -134,6 +96,101 @@ class MyHomePage extends GetView<HomeController> {
         ));
     // This trailing comma makes auto-formatting nicer for build methods.
   }
+
+  Padding user_detail() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          ClipOval(
+            child: Container(
+              height: 75,
+              width: 75,
+              color: Colors.grey.shade300,
+              child: Center(child: Text('data')),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text('WELCOME'),
+              Text('subtitle'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  card2() => Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey.shade300,
+          ),
+          height: 75,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text('data'),
+                  Text('-'),
+                ],
+              ),
+              VerticalDivider(
+                width: 10,
+              ),
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text('data'),
+                  Text('-'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+
+  card1() => Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey.shade300,
+          ),
+          height: 150,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              Text('data',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Divider(
+                height: 10,
+              ),
+              Text('123456789456123',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text('data'),
+            ],
+          ),
+        ),
+      );
 }
 
 Future<Null> signOut() async {
