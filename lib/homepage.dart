fa9fd44f2c47e8c7f804cb31e22f1c3655c5ead7
sk8rboil <sk8rboil008@windowslive.com>
2022-08-06 20:55:59 +0700

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_void_to_null, avoid_print, unused_local_variable, avoid_unnecessary_containers, non_constant_identifier_names, unnecessary_string_interpolations
+// ignore_for_file: prefer_const_constructors, prefer_void_to_null, avoid_print, unused_local_variable, avoid_unnecessary_containers, non_constant_identifier_names, unnecessary_string_interpolations, prefer_if_null_operators
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,14 +7,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:my_presence_app/controllers/home_controller.dart';
-import 'package:my_presence_app/controllers/signout_controller.dart';
 
 class MyHomePage extends GetView<HomeController> {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put((SignoutController()));
+    final controller = Get.put((HomeController()));
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -27,82 +27,105 @@ class MyHomePage extends GetView<HomeController> {
             ),
           ],
         ),
-        body: ListView(
-          padding: EdgeInsets.all(8.0),
-          children: <Widget>[
-            user_detail(),
-            card1(),
-            card2(),
-            Divider(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Text(
-                  'LAST 5 DAYS',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('see more'),
-                ),
-              ],
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey.shade300,
-                    ),
-                    height: 100,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          // ignore: prefer_const_literals_to_create_immutables
-                          children: <Widget>[
-                            Text(
-                              'DATA1',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              '${DateFormat.yMMMMEEEEd().format(DateTime.now())}',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        Text('${DateFormat.jms().format(DateTime.now())}'),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          // ignore: prefer_const_literals_to_create_immutables
-                          children: <Widget>[
-                            Text(
-                              'DATA2',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        Text('${DateFormat.jms().format(DateTime.now())}'),
-                      ],
-                    ),
+        body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          stream: controller.streamUser(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
+              Map<String, dynamic> user = snapshot.data!.data()!;
+
+              return ListView(
+                padding: EdgeInsets.all(8.0),
+                children: <Widget>[
+                  user_detail(user),
+                  card1(user),
+                  card2(),
+                  Divider(
+                    height: 10,
                   ),
-                );
-              },
-            ),
-          ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Text(
+                        'LAST 5 DAYS',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('see more'),
+                      ),
+                    ],
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey.shade300,
+                          ),
+                          height: 100,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: <Widget>[
+                                  Text(
+                                    'DATA1',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '${DateFormat.yMMMMEEEEd().format(DateTime.now())}',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                  '${DateFormat.jms().format(DateTime.now())}'),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: <Widget>[
+                                  Text(
+                                    'DATA2',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                  '${DateFormat.jms().format(DateTime.now())}'),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('NO DATA'),
+              );
+            }
+          },
         ),
         bottomNavigationBar: ConvexAppBar(
           style: TabStyle.fixedCircle,
@@ -119,7 +142,7 @@ class MyHomePage extends GetView<HomeController> {
     // This trailing comma makes auto-formatting nicer for build methods.
   }
 
-  Padding user_detail() {
+  Padding user_detail(user) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: Row(
@@ -129,7 +152,9 @@ class MyHomePage extends GetView<HomeController> {
               height: 75,
               width: 75,
               color: Colors.grey.shade300,
-              child: Center(child: Text('data')),
+              child: Center(
+                child: Text('data'),
+              ),
             ),
           ),
           SizedBox(
@@ -139,7 +164,7 @@ class MyHomePage extends GetView<HomeController> {
             // ignore: prefer_const_literals_to_create_immutables
             children: [
               Text('WELCOME'),
-              Text('subtitle'),
+              Text(user["username"]),
             ],
           ),
         ],
@@ -182,7 +207,7 @@ class MyHomePage extends GetView<HomeController> {
         ),
       );
 
-  card1() => Padding(
+  card1(user) => Padding(
         padding: const EdgeInsets.only(bottom: 8.0),
         child: Container(
           padding: EdgeInsets.all(20),
@@ -203,7 +228,7 @@ class MyHomePage extends GetView<HomeController> {
               Divider(
                 height: 10,
               ),
-              Text('123456789456123',
+              Text(user["email"],
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
